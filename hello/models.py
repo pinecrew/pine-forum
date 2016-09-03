1260ef56499dedc4f5ebb529f71728cbb38623ee
Vova Abdrakhmanov <369565@gmail.com>
2016-09-03 16:45:29 +0300

    maybe fix

--- hello/models.py
+++ hello/models.py
@@ -8,10 +8,10 @@ class Thread(models.Model):
     title = models.CharField(max_length=256)
 
     def topic(self):
-        return Message.objects.filter(thread==self).aggregate(models.Min('date'))
+        return Message.objects.filter(thread==self).aggregate(models.Min('date'))[0]
 
     def last(self):
-        return Message.objects.filter(thread==self).aggregate(models.Max('date'))
+        return Message.objects.filter(thread==self).aggregate(models.Max('date'))[0]
 
 class Message(models.Model):
     author = models.CharField(max_length=256)
