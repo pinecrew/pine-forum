2138211467b127e603b06f90f9d51a44181496cd
Vova Abdrakhmanov <369565@gmail.com>
2016-09-03 16:09:36 +0300

    add messages and threads

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+    pass
--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,15 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Thread(models.Model):
+    def topic(self):
+        Message.objects.filter(thread==self).aggregate(models.Min('date'))
+    def last(self):
+        Message.objects.filter(thread==self).aggregate(models.Max('date'))
+
+class Message(models.Model):
+    author = models.CharField(max_length=256)
+    text = models.TextField()
+    date = models.DateTimeField('date created', auto_now_add=True)
+    thread = models.ForeignKey('Thread', on_delete=models.CASCADE)
