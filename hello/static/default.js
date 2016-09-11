c614872232507b4ac819edfad50c7585ad8658b9
Ilya Chechetkin <illaech@gmail.com>
2016-09-11 16:16:06 +0300

    csrf token & absent '>'

--- hello/static/default.js
+++ hello/static/default.js
@@ -1,7 +1,7 @@
 // toggles visibility of element
 toggle_visibility = function(id) {
     elem = document.getElementById(id);
-    if (elem.className.indexOf('invisible')  -1) {
+    if (elem.className.indexOf('invisible') > -1) {
         elem.className = elem.className.replace('invisible', '');
     } else {
         elem.className += ' invisible';
