51ab6be1f10f531a597e45316ddc269539520c3a
Ilya Chechetkin <illaech@gmail.com>
2016-09-11 19:13:26 +0300

    fade in while signing in

--- hello/static/default.js
+++ hello/static/default.js
@@ -2,6 +2,10 @@
 toggle_visibility = function(id) {
     elem = document.getElementById(id);
     if (elem.className.indexOf('invisible') > -1) {
+        a = document.createElement("a");
+        a.className = "fade-in";
+        a.onclick = function() { document.body.removeChild(a); toggle_visibility(id) };
+        document.body.appendChild(a);
         elem.className = elem.className.replace('invisible', '');
     } else {
         elem.className += ' invisible';
