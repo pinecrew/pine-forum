4986d0626804161afa420d486f43ae223976c3bf
Ilya Chechetkin <illaech@gmail.com>
2016-09-24 14:52:32 +0300

    thread_title in admin

--- hello/admin.py
+++ hello/admin.py
@@ -3,7 +3,10 @@ from .models import Message, Thread
 # Register your models here.
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('author', 'preview', 'thread.title', 'time')
+    list_display = ('author', 'preview', 'thread_title', 'time')
+
+    def thread_title(self, instance):
+        return instance.thread.title
 
 @admin.register(Thread)
 class ThreadAdmin(admin.ModelAdmin):
