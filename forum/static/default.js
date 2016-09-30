960dfc80aa778bd4a7a26c60da813c28ed0a13f3
Ilya Chechetkin <illaech@gmail.com>
2016-09-30 23:14:25 +0300

    update default.js

--- forum/static/default.js
+++ forum/static/default.js
@@ -1,44 +1,41 @@
 // toggles visibility of the element by id
 // input = id of element which visibility will be toggled
-toggle_visibility = function(id) {
-    var elem = document.getElementById(id);
-    if (elem.className.indexOf('invisible') > -1) {
-        elem.className = elem.className.replace('invisible', '');
-        return true;
-    } else {
-        elem.className = (elem.className + ' invisible').replace('  ', ' ');
-        return false;
-    }
+toggle_visibility_id = function(id) {
+    var el = document.querySelector('#' + id);
+    return toggle_visibility(el);
 }
 
 // toggles visibility of the element
 // input = element which visibility will be toggled
-toggle_visibility_elem = function(elem) {
-    if (elem.className.indexOf('invisible') > -1) {
-        elem.className = elem.className.replace('invisible', '');
-        return true;
+toggle_visibility = function(el) {
+    if (el.className.indexOf('invisible') > -1) {
+        el.className = el.className.replace('invisible', '');
+        return true; // become visible
     } else {
-        elem.className = (elem.className + ' invisible').replace('  ', ' ');
-        return false;
+        el.className = (el.className + ' invisible').replace('  ', ' ');
+        return false; // become invisible
     }
 }
 
 // fade-in by creating a link
 // id = id of element which will be shown in front of link
 fade_in = function(id) {
-    if (toggle_visibility(id)) {
+    if (toggle_visibility_id(id)) {
         a = document.createElement("a");
         a.className = "fade-in";
-        a.onclick = function() { document.body.removeChild(a); toggle_visibility(id); return false; };
+        a.onclick = function() { document.body.removeChild(a); toggle_visibility_id(id); return false; };
         document.body.appendChild(a);
     }
 }
 
 edit_message = function(id) {
-    frm = document.getElementById('form_' + id);
-    toggle_visibility_elem(frm);
-    var elems = document.getElementById('m' + id).querySelectorAll('div.text > *:not(header)');
-    for (var i = 0; i < elems.length; i++) {
-        toggle_visibility_elem(elems[i]);        
+    var form = document.querySelector('#form_' + id);
+    var els = document.querySelectorAll('#m' + id + ' div.text > *:not(header)');
+    for (var i = 0; i < els.length; i++) {
+        toggle_visibility(els[i]);
+    }
+    if (toggle_visibility(form)) {
+        var textarea = form.querySelector('textarea');
+        textarea.style.height = textarea.scrollHeight + 'px';
     }
 }
