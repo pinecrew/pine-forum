41db7405154c0368358db5076263dba76356860e
Ilya Chechetkin <illaech@gmail.com>
2016-10-02 18:43:08 +0300

    fucking slash

--- forum/static/default.js
+++ forum/static/default.js
@@ -3,7 +3,7 @@
 toggle_visibility_id = function(id) {
     var el = document.querySelector('#' + id);
     return toggle_visibility(el);
-}
+};
 
 // toggles visibility of the element
 // input = element which visibility will be toggled
@@ -15,7 +15,7 @@ toggle_visibility = function(el) {
         el.className = (el.className + ' invisible').replace('  ', ' ');
         return false; // become invisible
     }
-}
+};
 
 // fade-in by creating a link
 // id = id of element which will be shown in front of link
@@ -26,45 +26,15 @@ fade_in = function(id) {
         a.onclick = function() { document.body.removeChild(a); toggle_visibility_id(id); return false; };
         document.body.appendChild(a);
     }
-}
+};
+
+var div_backup = '';
 
 edit_message = function(id) {
     var div = document.querySelector('#div' + id + ' .content');
     var wrapper = document.querySelector('#div' + id + ' .text');
-    div.backup = div.innerHTML;
-    
-    var ajax = false;
-    if (window.XMLHttpRequest) {
-        ajax = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-        ajax = new ActiveXObject('Microsoft.XMLHTTP');
-    }
-    
-    if (ajax) {
-        ajax.open('GET', '/message/' + id);
-        
-        ajax.onreadystatechange = function () {
-            if (ajax.readyState == 4 && ajax.status == 200) {
-                div.contentEditable = true;
-                div.innerHTML = ajax.responseText;
-                wrapper.innerHTML += '<div class="controls">' +
-                    '<a href="#" onclick="save_message(' + id + ', true); return false;" />Сохранить</a>' +
-                    '<a href="#" onclick="save_message(' + id + ', false); return false;" />Отменить</a>' +
-                    '<div/>';
-                div.focus();
-            }
-        }
-        
-        ajax.send(null);
-    }
-}
-
-save_message = function(id, send) {
-    var div = document.querySelector('#div' + id + ' .content');
-    var wrapper = document.querySelector('#div' + id + ' .text');
-    if (send) {
-        wrapper.removeChild(wrapper.querySelector('.controls'));
-        var text = div.innerHTML;
+    if (!wrapper.querySelector('.controls')) {
+        div_backup = div.innerHTML;
         
         var ajax = false;
         if (window.XMLHttpRequest) {
@@ -74,19 +44,54 @@ save_message = function(id, send) {
         }
         
         if (ajax) {
-            ajax.open('POST', '/message/' + id);
-            ajax.setRequestHeader('Content-type', 'text/plaintext');
+            ajax.open('GET', '/message/' + id);
+            
+            ajax.onreadystatechange = function () {
+                if (ajax.readyState == 4 && ajax.status == 200) {
+                    div.contentEditable = true;
+                    div.innerText = ajax.responseText;
+                    wrapper.innerHTML += '<div class="controls">' +
+                        '<a href="#" onclick="save_message(' + id + ', true); return false;" />Сохранить</a>' +
+                        '<a href="#" onclick="save_message(' + id + ', false); return false;" />Отменить</a>' +
+                        '<div/>';
+                    div.focus();
+                }
+            };
+            
+            ajax.send(null);
+        }
+    }
+};
+
+save_message = function(id, send) {
+    var div = document.querySelector('#div' + id + ' .content');
+    var wrapper = document.querySelector('#div' + id + ' .text');
+    if (send) {
+        wrapper.removeChild(wrapper.querySelector('.controls'));
+        var text = div.innerText;
+        
+        var ajax = false;
+        if (window.XMLHttpRequest) {
+            ajax = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            ajax = new ActiveXObject('Microsoft.XMLHTTP');
+        }
+        
+        if (ajax) {
+            ajax.open('POST', '/message/' + id + '/');
+            ajax.setRequestHeader('Content-Type', 'text/plain; charset=utf-8');
             
             ajax.onreadystatechange = function () {
                 if (ajax.readyState == 4 && ajax.status == 200) {
                     div.innerHTML = ajax.responseText;
+                    console.log(ajax.responseText);
                 }
-            }
+            };
             
-            ajax.send(text);
+            ajax.send({'message_text': text});
         }
     } else {
-        div.innerHTML = div.backup;
+        div.innerHTML = div_backup;
     }
     div.contentEditable = false;
-}
+};
