b5dc092b5b06560771d5dfc3cf4ca0956377814c
Vova Abdrakhmanov <369565@gmail.com>
2016-10-02 18:20:19 +0300

    add decorator for making POST possible!

--- forum/views.py
+++ forum/views.py
@@ -35,12 +35,12 @@ def logout(request):
         pass # there's no user here
     return redirect('/')
 
+@csrf_exempt
 def message(request, message_id):
     m = Message.objects.get(id=message_id)
     if request.method == 'GET':
         return HttpResponse(m.text, 'text/plain')
     elif request.method == 'POST':
-        return redirect('/')
         m.text = request.POST['message_text']
         m.save()
         return HttpResponse(m.html(), 'text/html')
