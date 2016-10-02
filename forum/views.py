4bafff1881acf746578ed281392e3f98f0f13f1c
Vova Abdrakhmanov <369565@gmail.com>
2016-10-02 18:23:25 +0300

    fix forgotten import

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Thread, Message
 
