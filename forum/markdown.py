f0b7b4f8074b6e10f96f185250b9aa7772c31927
Vova Abdrakhmanov <369565@gmail.com>
2021-07-10 23:02:36 +0300

    Fix typo

--- forum/markdown.py
+++ forum/markdown.py
@@ -8,7 +8,7 @@ User = get_user_model()
 
 def render_html(text):
     mentions = set(re.findall(r'\B@.+?\b', text))
-    usernames = User.objects.filter(username__in=[i[1:] for m in mentions]).values_list('username', flat=True)
+    usernames = User.objects.filter(username__in=[m[1:] for m in mentions]).values_list('username', flat=True)
     for username in usernames:
         mention = f'@{username}'
         text = text.replace(mention, f'<a href="/user/{username}/">{mention}</a>')
